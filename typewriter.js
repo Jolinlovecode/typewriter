b314f449b339b6451ad0e054033a72421e197c25
Hongling Zhang <jolinhlzhang@email.com>
2022-01-18 23:16:37 -0700

    Animated Sentence

--- typewriter.js
+++ typewriter.js
@@ -2,7 +2,7 @@ const sentence = "hello there from lighthouse labs\n";
 setTimeout(() => {
     for (const char of sentence) {
     process.stdout.write(char);
-    }
+    } console.log("");
   }, 5000);
   
 
